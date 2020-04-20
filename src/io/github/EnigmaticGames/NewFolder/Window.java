564674a42ce8a8c3a38b179efad5737fec250a03
Johnny Stene <jhonnystene@gmail.com>
2020-04-20 16:19:08 -0600

    Added camera

--- src/io/github/EnigmaticGames/NewFolder/Window.java
+++ src/io/github/EnigmaticGames/NewFolder/Window.java
@@ -10,8 +10,10 @@ import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
 public class Window extends JFrame {
-	BufferedImage frameBuffer;
-	Keyboard keyManager;
+	public BufferedImage frameBuffer;
+	public Keyboard keyManager;
+	public int cameraX;
+	public int cameraY;
 	
 	public Window(int width, int height) {
 		super();
@@ -25,7 +27,7 @@ public class Window extends JFrame {
 		frameBuffer = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 	}
 	
-	public void drawImage(BufferedImage image, int x, int y) {
+	public void drawImageUI(BufferedImage image, int x, int y) {
 		Graphics2D graphics = frameBuffer.createGraphics();
 		graphics.drawImage(image, x, y, this);
 		graphics.dispose();
@@ -37,5 +39,21 @@ public class Window extends JFrame {
 		graphics.setColor(Color.WHITE);
 		graphics.fillRect(0, 0, frameBuffer.getWidth(), frameBuffer.getHeight());
 		graphics.dispose();
+	}
+	
+	public void drawImageCamera(BufferedImage image, int x, int y) {
+		Graphics2D graphics = frameBuffer.createGraphics();
+		graphics.drawImage(image, x - cameraX, y - cameraY, this);
+		graphics.dispose();
+	}
+	
+	public void centerCameraOn(CollisionItem item) {
+		cameraX = item.x - 400 - (item.width / 2);
+		cameraY = item.y - 300 - (item.height / 2);
+	}
+	
+	public void centerCameraOn(int x, int y) {
+		cameraX = x - 400;
+		cameraY = y - 300;
 	}
 }
